be65a21ea4794e4c0299e020dcf3e87b506a12b3
vikramDrai <118328927+vikramDrai@users.noreply.github.com>
2022-11-28 13:21:14 +0545

    Add files via upload

---
+++ Const.java
@@ -0,0 +1,12 @@
+public class Const {
+
+    Const() {
+        System.out.println("this is my first constructor");
+    }
+
+    public static  void main(String[] args) {
+        Const ba = new Const();
+        
+    }
+
+}
