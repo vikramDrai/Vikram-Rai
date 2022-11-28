2d091bdad93f535e8d86fa0dc3d72fa3ddf60304
vikramDrai <118328927+vikramDrai@users.noreply.github.com>
2022-11-28 13:22:30 +0545

    Add files via upload

---
+++ overloading.java
@@ -0,0 +1,20 @@
+public class overloading {
+    public static  int sum(int a, int b)
+    {
+        return a+b;
+
+    }
+    public static int sum(int a, int b, int c)
+    {
+ 
+        return a+b+c;
+        
+    }
+    public static void main(String[] args)
+    
+    {
+        
+        System.out.println( sum(2,3));
+        System.out.println( sum(5,10,15));
+    }
+}
